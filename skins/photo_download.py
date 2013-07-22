ec42482bea99052d631ecf6062f2a7e1e8376091
Benoît Pin <pin@cri.ensmp.fr>
2013-07-22 15:49:43 +0200

    Nom du fichier original lors du download.

--- skins/photo_download.py
+++ skins/photo_download.py
@@ -1,5 +1,6 @@
 req = context.REQUEST
 resp = req.RESPONSE
-resp.setHeader('Content-Disposition', 'attachment; filename=%s' % context.getId())
+filename = getattr(context, 'orig_name', context.getId())
+resp.setHeader('Content-Disposition', 'attachment; filename=%s' % filename)
 
 return context.index_html(req, resp)
