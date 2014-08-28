5e9d5a6ac7b22dbb732acd070728ae8e079ac2ff
Benoît Pin <pin@cri.ensmp.fr>
2014-08-28 14:46:22 +0200

    Factorisation / bugfix.

--- skins/getSelectionPhotosInfos.py
+++ skins/getSelectionPhotosInfos.py
@@ -1,8 +1,6 @@
 ##parameters=pho_start=None, batch_size=None
 from Products.CMFCore.utils import getToolByName
 from Products.Plinn.PloneMisc import Batch
-from Products.Portfolio.utils import translate
-def _(message) : return translate(message, context).encode('utf-8')
 
 uidh = getToolByName(context, 'portal_uidhandler')
 utool = getToolByName(context, 'portal_url')
@@ -28,8 +26,9 @@ else :
 		selection = req.SESSION.get('objects_selection', [])
 
 
-start = pho_start if pho_start is not None else 0
 brains = [uidh.getBrain(uid) for uid in selection]
+start = pho_start if pho_start is not None else 0
+batch_size = batch_size if batch_size is not None else context.default_batch_size
 batch = Batch(brains, batch_size, start, quantumleap=1)
 cart = req.SESSION.get('cart', None)
 
