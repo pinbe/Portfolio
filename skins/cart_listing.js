fbdbc13c9f32f9df94087780ce90bc0916bde420
Benoît Pin <pin@cri.ensmp.fr>
2014-10-10 13:00:26 +0200

    jslint

--- skins/cart_listing.js
+++ skins/cart_listing.js
@@ -9,39 +9,39 @@ var CartListing;
 (function(){
 var ENTERKEY = 13;
 
-CartListing = function(table) {
+CartListing = function(table) {
 	var thisCL = this;
 	this.table = table;
 	var form = table.parentNode;
-	while(form.tagName != 'FORM')
-		form = form.parentNode;
+	while(form.tagName !== 'FORM') {
+		form = form.parentNode;}
 	
 	this.fm = new FormManager(form);
 	
-	if (browser.isIE)
-		addListener(table, 'focusout', function(evt){thisCL.updateRow(evt);});
-	else
-		addListener(table, 'change', function(evt){thisCL.updateRow(evt);});
+	if (browser.isIE){
+		addListener(table, 'focusout', function(evt){thisCL.updateRow(evt);});}
+	else{
+		addListener(table, 'change', function(evt){thisCL.updateRow(evt);});}
 	
 	addListener(table, 'keypress', function(evt){thisCL.onKeypress(evt);});
-}
+};
 
 CartListing.prototype.updateRow = function(evt) {
 	var target = getTargetedObject(evt);
-	if (target.tagName != 'INPUT')
-		return;
+	if (target.tagName !== 'INPUT'){
+		return;}
 			
 	
 	var row = target.parentNode.parentNode;
-	var inputs = row.getElementsByTagName('input')
+	var inputs = row.getElementsByTagName('input');
 	var refreshBtn = inputs[1];
 	this.fm.submitButton = refreshBtn;
 	this.fm.submit(evt);
 };
 
 CartListing.prototype.onKeypress = function(evt){
-	if (evt.keyCode == ENTERKEY)
-		this.updateRow(evt);
+	if (evt.keyCode === ENTERKEY){
+		this.updateRow(evt);}
 };
 
-})();
+}());
